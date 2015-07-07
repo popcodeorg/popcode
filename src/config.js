b6e94568e320be9f2424570f31b297d22e25fea2
Mat Brown <mat@genius.com>
2015-07-06 21:02:21 -0400

    Add a doctype to the default HTML

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   defaults: {
-    html: "<html>\n  <head>\n    <title>Page Title</title>\n  </head>\n  <body>\n    <!-- Put your page markup here -->\n  </body>\n</html>",
+    html: "<!doctype html>\n<html>\n  <head>\n    <title>Page Title</title>\n  </head>\n  <body>\n    <!-- Put your page markup here -->\n  </body>\n</html>",
     css: "",
     javascript: ""
   }
