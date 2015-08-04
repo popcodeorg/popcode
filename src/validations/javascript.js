2fe1b11832a91a2a52f01a42e98aaf408e545507
kinduff <abarcadabra@gmail.com>
2015-08-03 21:56:31 -0500

    JS verbose translation keys

--- src/validations/javascript.js
+++ src/validations/javascript.js
@@ -24,69 +24,69 @@ var match = {
 
 var humanErrors = {
   "E019": function(error) {
-    return i18n.t("javascript.E019", { error_a: error.a, error_a_match: match[error.a] });
+    return i18n.t("javascript.unmatched", { error_a: error.a, error_a_match: match[error.a] });
   },
 
   "E020": function(error) {
-    return i18n.t("javascript.E019", { error_a: error.a, error_b: error.b });
+    return i18n.t("javascript.closing-match", { error_a: error.a, error_b: error.b });
   },
 
   "E030": function() {
-    return i18n.t("javascript.E030");
+    return i18n.t("javascript.expected-identifier");
   },
 
   "W003": function(error) {
-    return i18n.t("javascript.E030", { error_a: error.a });
+    return i18n.t("javascript.undefined-variable", { error_a: error.a });
   },
 
   "W030": function() {
-    return i18n.t("javascript.W030");
+    return i18n.t("javascript.unexpected-expression");
   },
 
   "W031": function() {
-    return i18n.t("javascript.W031");
+    return i18n.t("javascript.use-new-object");
   },
 
   "W032": function() {
-    return i18n.t("javascript.W032");
+    return i18n.t("javascript.unnecessary-semicolon");
   },
 
   "W033": function() {
-    return i18n.t("javascript.W033");
+    return i18n.t("javascript.missing-semicolon");
   },
 
   "W058": function(error) {
-    return i18n.t("javascript.W058", { error_a: error.a } );
+    return i18n.t("javascript.missing-parentheses", { error_a: error.a } );
   },
 
   "W084": function() {
-    return i18n.t("javascript.W084");
+    return i18n.t("javascript.strict-comparison-operator");
   },
 
   "W098": function(error) {
-    return i18n.t("javascript.W098", { error_a: error.a });
+    return i18n.t("javascript.unused-variable", { error_a: error.a });
   },
 
   "W112": function() {
-    return i18n.t("javascript.W112");
+    return i18n.t("javascript.unclosed-string");
   },
 
   "W116": function(error) {
     if (error.a === "===" && error.b === "==") {
-      return i18n.t("javascript.W116.case-1");
+      return i18n.t("javascript.strict-operators.equal");
     } else if (error.a === "!==" && error.b === "!=") {
-      return i18n.t("javascript.W116.case-2");
+      return i18n.t("javascript.strict-operators.different");
     } else {
-      return i18n.t("javascript.W116.case-3", { error_a: error.a, error_b: error.b });
+      return i18n.t("javascript.strict-operators.custom-case", { error_a: error.a, error_b: error.b });
     }
   },
 
   "W117": function(error) {
-    i18n.t("javascript.W117", { error_a: error.a });
+    i18n.t("javascript.declare-variable", { error_a: error.a });
   },
 
   "W123": function(error) {
-    i18n.t("javascript.W123", { error_a: error.a });
+    i18n.t("javascript.duplicated-declaration", { error_a: error.a });
   }
 };
 
