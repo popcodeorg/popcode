8ad637cfdbdc2d351367b8df5d2f5c66d45a8940
kinduff <abarcadabra@gmail.com>
2015-08-04 12:28:24 -0500

    Moves i18next init to application.js

--- src/application.js
+++ src/application.js
@@ -1,6 +1,12 @@
 "use strict";
-
 var React = require('react');
+var i18n = require('i18next-client');
 var Workspace = require('./components/workspace.jsx');
+
+var i18n_options = {
+  fallbackLng: 'en',
+  debug: true
+}
+i18n.init(i18n_options);
 
 React.render(React.createElement(Workspace), document.getElementById('main'));
--- src/validations/index.js
+++ src/validations/index.js
@@ -1,9 +1,3 @@
-var i18n = require('i18next-client');
-i18n.init({
-  fallbackLng: 'en',
-  debug: true
-});
-
 module.exports = {
   html: require('./html.js'),
   css: require('./css.js'),
