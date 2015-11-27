8768363bc08a1cae507645574dc44f6a1c320c61
Mat Brown <mat.a.brown@gmail.com>
2015-11-27 09:06:45 -0500

    Second test for behavior on changing current project key

--- src/reducers/__tests__/currentProject-test.js
+++ src/reducers/__tests__/currentProject-test.js
@@ -3,17 +3,29 @@
 
 jest.dontMock('../currentProject');
 
+var Immutable = require('immutable');
+
 describe('currentProject', function() {
   var currentProject = require('../currentProject');
 
-  describe('with no initial project key', function() {
-    var action = {
-      type: 'CURRENT_PROJECT_LOADED_FROM_STORAGE',
-      payload: {project: {projectKey: '12345'}},
-    };
+  var action = {
+    type: 'CURRENT_PROJECT_LOADED_FROM_STORAGE',
+    payload: {project: {projectKey: '12345'}},
+  };
 
+  describe('with no initial project key', function() {
     it('should set current project key', function() {
       expect(currentProject(undefined, action).toJS()).toEqual(
+        {projectKey: action.payload.project.projectKey}
+      );
+    });
+  });
+
+  describe('with initial project key', function() {
+    it('should change current project key', function() {
+      var previousState = Immutable.fromJS({projectKey: '1'});
+
+      expect(currentProject(previousState, action).toJS()).toEqual(
         {projectKey: action.payload.project.projectKey}
       );
     });
