cc4038815cf2588caebe11f7a2b6f91122801d6f
Mat Brown <mat.a.brown@gmail.com>
2015-11-29 10:19:26 -0500

    Reducers for errors

---
+++ src/reducers/__tests__/errors-test.js
@@ -0,0 +1,50 @@
+/* eslint-env jest */
+/* eslint global-require: 0 */
+
+jest.dontMock('../errors');
+
+var Immutable = require('immutable');
+
+describe('errors', function() {
+  var errors = require('../errors');
+
+  describe('unknown action', function() {
+    var action = {type: 'BOGUS'}
+
+    it('should return previous state', function() {
+      var stateIn = new Immutable.Map();
+      expect(errors(stateIn, action)).toBe(stateIn);
+    });
+  });
+
+  describe('VALIDATING_SOURCE', function() {
+    var action = {
+      type: 'VALIDATING_SOURCE',
+      language: 'javascript',
+    };
+
+    it('should provide empty map from undefined state', function() {
+      expect(Immutable.Map.isMap(errors(undefined, action))).toBeTruthy();
+    });
+
+    it('should remove errors', function() {
+      var stateIn = Immutable.fromJS({html: [], css: [], javascript: []});
+      expect(errors(stateIn, action).has('javascript')).toBeFalsy();
+    });
+  });
+
+  describe('VALIDATED_SOURCE', function() {
+    var error = {};
+
+    var action = {
+      type: 'VALIDATED_SOURCE',
+      language: 'javascript',
+      errors: [error],
+    };
+
+    it('should set errors where there were none', function() {
+      var stateIn = Immutable.fromJS({html: [], css: []});
+      expect(errors(stateIn, action).get('javascript').get(0)).toBe(error);
+    });
+  });
+});
---
+++ src/reducers/errors.js
@@ -0,0 +1,21 @@
+var Immutable = require('immutable');
+
+function errors(stateIn, action) {
+  var state = stateIn;
+  if (state === undefined) {
+    state = new Immutable.Map();
+  }
+
+  switch (action.type) {
+    case 'VALIDATING_SOURCE':
+      return state.delete(action.language);
+
+    case 'VALIDATED_SOURCE':
+      return state.set(action.language, new Immutable.List(action.errors));
+
+    default:
+      return state;
+  }
+}
+
+module.exports = errors;
