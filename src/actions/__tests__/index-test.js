b2af326ab5824ed16f8a58fd4787a6dedfec5f50
Mat Brown <mat.a.brown@gmail.com>
2015-12-20 21:13:55 -0500

    loadCurrentProjectFromStorage action

--- src/actions/__tests__/index-test.js
+++ src/actions/__tests__/index-test.js
@@ -30,4 +30,83 @@ describe('errors', function() {
       expect(this.dispatchedAction.payload.projectKey).toBeDefined();
     });
   });
+
+  describe('loadCurrentProjectFromStorage with project', function() {
+    var project = {
+      projectKey: '12345',
+      sources: {html: '', css: '', javascript: ''},
+      libraries: [],
+    };
+
+    var Storage = require('../../services/Storage');
+
+    beforeEach(function() {
+      var loadCurrentProjectFromStorageFn =
+        Actions.loadCurrentProjectFromStorage();
+
+      Storage.getCurrentProjectKey.mockReturnValue(
+        Promise.resolve(project.projectKey)
+      );
+      Storage.load.mockReturnValue(Promise.resolve(project));
+      loadCurrentProjectFromStorageFn(dispatch);
+
+      this.actionsDispatched = new Promise(dispatch.mockImplementation).
+        then(function() {
+          return {
+            projectLoaded: dispatch.mock.calls[0][0],
+            currentProjectChanged: dispatch.mock.calls[1][0],
+          };
+        });
+    });
+
+    pit('should dispatch PROJECT_LOADED_FROM_STORAGE', function() {
+      return this.actionsDispatched.then(function(actions) {
+        expect(actions.projectLoaded.type).
+          toBe('PROJECT_LOADED_FROM_STORAGE');
+      });
+    });
+
+    pit('should pass project to PROJECT_LOADED_FROM_STORAGE', function() {
+      return this.actionsDispatched.then(function(actions) {
+        expect(actions.projectLoaded.payload.project).toBe(project);
+      });
+    });
+
+    pit('should dispatch CURRENT_PROJECT_CHANGED', function() {
+      return this.actionsDispatched.then(function(actions) {
+        expect(actions.currentProjectChanged.type).
+          toBe('CURRENT_PROJECT_CHANGED');
+      });
+    });
+
+    pit('should dispatch CURRENT_PROJECT_CHANGED', function() {
+      return this.actionsDispatched.then(function(actions) {
+        expect(actions.currentProjectChanged.payload.projectKey).
+          toBe(project.projectKey);
+      });
+    });
+  });
+
+  describe('loadCurrentProjectFromStorage with no project', function() {
+    var Storage = require('../../services/Storage');
+
+    beforeEach(function() {
+      var loadCurrentProjectFromStorageFn =
+        Actions.loadCurrentProjectFromStorage();
+
+      Storage.getCurrentProjectKey.mockReturnValue(Promise.resolve(undefined));
+      loadCurrentProjectFromStorageFn(dispatch);
+
+      this.actionsDispatched = new Promise(dispatch.mockImplementation).
+        then(function() {
+          return dispatch.mock.calls[0][0];
+        });
+    });
+
+    pit('should dispatch PROJECT_CREATED action', function() {
+      return this.actionsDispatched.then(function(action) {
+        expect(action.type).toBe('PROJECT_CREATED');
+      });
+    });
+  });
 });
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+var Storage = require('../services/Storage');
+
 function generateProjectKey() {
   var date = new Date();
   return (date.getTime() * 1000 + date.getMilliseconds()).toString();
@@ -10,6 +12,27 @@ exports.createProject = function() {
       payload: {
         projectKey: generateProjectKey(),
       },
+    });
+  };
+};
+
+exports.loadCurrentProjectFromStorage = function() {
+  return function(dispatch) {
+    Storage.getCurrentProjectKey().then(function(projectKey) {
+      if (projectKey) {
+        Storage.load(projectKey).then(function(project) {
+          dispatch({
+            type: 'PROJECT_LOADED_FROM_STORAGE',
+            payload: {project: project},
+          });
+          dispatch({
+            type: 'CURRENT_PROJECT_CHANGED',
+            payload: {projectKey: projectKey},
+          });
+        });
+      } else {
+        exports.createProject()(dispatch);
+      }
     });
   };
 };
