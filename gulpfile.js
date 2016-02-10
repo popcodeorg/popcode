518e232316938e997e5ad0c9fbea15174af10821
Mat Brown <mat.a.brown@gmail.com>
2016-02-09 22:21:05 -0500

    Block server request until browserify task is complete
    Could be a little more graceful about it

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var browserSync = require('browser-sync').create();
-var watchify = require('watchify');
 var browserify = require('browserify-incremental');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
@@ -18,6 +17,8 @@ var srcDir = 'src';
 var baseDir = 'static';
 var distDir = baseDir + '/compiled';
 var stylesheetsDir = srcDir + '/css';
+
+var browserifyDone = Promise.resolve();
 
 var browserifyOpts = {
   entries: ['src/application.js'],
@@ -38,11 +39,19 @@ gulp.task('css', function() {
 // https://www.npmjs.com/package/watchify-request
 
 gulp.task('js', function() {
-  return browserifyCompiler.bundle().
-    pipe(source('application.js')).
-    pipe(buffer()).
-    pipe(gulp.dest(distDir)).
-    pipe(browserSync.reload({stream: true}));
+  var stream;
+
+  browserifyDone = new Promise(function(resolve, reject) {
+    stream = browserifyCompiler.bundle().
+      pipe(source('application.js')).
+      pipe(buffer()).
+      pipe(gulp.dest(distDir)).
+      pipe(browserSync.reload({stream: true})).
+      on('end', resolve).
+      on('error', reject);
+  });
+
+  return stream;
 });
 
 gulp.task('watch', ['browserSync', 'css', 'js'], function() {
@@ -54,6 +63,9 @@ gulp.task('browserSync', function() {
   browserSync.init({
     server: {
       baseDir: baseDir,
+      middleware: function(_req, _res, next) {
+        browserifyDone.then(next);
+      },
     },
   });
 });
