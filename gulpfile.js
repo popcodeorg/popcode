2aa3481a21341a807ccafd4bfbc75b41c9d8bdc6
Mat Brown <mat.a.brown@gmail.com>
2016-02-10 21:20:06 -0500

    Minification and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,13 @@ var browserSync = require('browser-sync').create();
 var browserify = require('browserify-incremental');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var cssnano = require('gulp-cssnano');
 var gutil = require('gulp-util');
-var assign = require('lodash/assign');
 var reactify = require('reactify');
 var brfs = require('brfs');
 var envify = require('envify');
-
-//TODO sourcemaps
-//TODO production build (including minification)
 
 var srcDir = 'src';
 var baseDir = 'static';
@@ -24,19 +23,19 @@ var browserifyOpts = {
   entries: ['src/application.js'],
   extensions: ['.jsx'],
   transform: [reactify, brfs, envify],
+  debug: true,
 };
 var browserifyCompiler = browserify(browserifyOpts);
-// var browserifyWatcher = watchify(browserifyCompiler);
 
 gulp.task('css', function() {
   return gulp.src(stylesheetsDir + '/**/*.css').
     pipe(concat('application.css')).
+    pipe(sourcemaps.init({loadMaps: true})).
+    pipe(gutil.env.production ? cssnano() : gutil.noop()).
+    pipe(sourcemaps.write('./')).
     pipe(gulp.dest(distDir)).
     pipe(browserSync.reload({stream: true}));
 });
-
-// https://github.com/gulpjs/gulp/blob/master/docs/recipes/fast-browserify-builds-with-watchify.md
-// https://www.npmjs.com/package/watchify-request
 
 gulp.task('js', function() {
   var stream;
@@ -45,6 +44,9 @@ gulp.task('js', function() {
     stream = browserifyCompiler.bundle().
       pipe(source('application.js')).
       pipe(buffer()).
+      pipe(sourcemaps.init({loadMaps: true})).
+      pipe(gutil.env.production ? uglify() : gutil.noop()).
+      pipe(sourcemaps.write('./')).
       pipe(gulp.dest(distDir)).
       pipe(browserSync.reload({stream: true})).
       on('end', resolve).
