497a74d849291f3b3c82a73750968852cdd28ca3
Mat Brown <mat.a.brown@gmail.com>
2016-04-04 19:28:50 -0400

    Initialize i18n in actual application

--- src/application.js
+++ src/application.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import Immutable from 'immutable';
 import installDevTools from 'immutable-devtools';
 import Application from './components/Application';
+import initI18n from './util/initI18n';
 import 'babel-polyfill';
 
 installDevTools(Immutable);
+
+initI18n();
 
 ReactDOM.render(
   React.createElement(Application),
