06be9fc524f3904e8ae880803528915787305886
Mat Brown <mat.a.brown@gmail.com>
2016-05-07 17:49:05 -0400

    Upgrade jsdom

--- test/util/renderApplication.js
+++ test/util/renderApplication.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Application from '../../src/components/Application';
 import TestUtils from 'react-addons-test-utils';
-import $R from './rquery';
 
-export default () => $R(TestUtils.renderIntoDocument(<Application/>));
+export default () => TestUtils.renderIntoDocument(<Application/>);
