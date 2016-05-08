7c9216037347250411b9449cd3736c4889fb81ed
Mat Brown <mat.a.brown@gmail.com>
2016-05-08 14:02:42 -0400

    Fix lint in ErrorList

--- src/components/ErrorList.jsx
+++ src/components/ErrorList.jsx
@@ -3,44 +3,42 @@ import classnames from 'classnames';
 import get from 'lodash/get';
 import ErrorSublist from './ErrorSublist';
 
-class ErrorList extends React.Component {
-  render() {
-    const docked = get(this, 'props.docked', false);
+function ErrorList(props) {
+  const docked = get(this, 'props.docked', false);
 
-    return (
-      <div className={
-        classnames(
-          'errorList',
-          'output-item',
-          {'errorList--docked': docked, 'output-item--shrink': docked}
-        )}
-      >
-        <ErrorSublist
-          language="html"
-          errors={this.props.html}
-          onErrorClicked={this.props.onErrorClicked}
-        />
-        <ErrorSublist
-          language="css"
-          errors={this.props.css}
-          onErrorClicked={this.props.onErrorClicked}
-        />
-        <ErrorSublist
-          language="javascript"
-          errors={this.props.javascript}
-          onErrorClicked={this.props.onErrorClicked}
-        />
-      </div>
-    );
-  }
+  return (
+    <div
+      className={classnames(
+        'errorList',
+        'output-item',
+        {'errorList--docked': docked, 'output-item--shrink': docked}
+      )}
+    >
+      <ErrorSublist
+        errors={props.html}
+        language="html"
+        onErrorClicked={props.onErrorClicked}
+      />
+      <ErrorSublist
+        errors={props.css}
+        language="css"
+        onErrorClicked={props.onErrorClicked}
+      />
+      <ErrorSublist
+        errors={props.javascript}
+        language="javascript"
+        onErrorClicked={props.onErrorClicked}
+      />
+    </div>
+  );
 }
 
 ErrorList.propTypes = {
-  html: React.PropTypes.array.isRequired,
   css: React.PropTypes.array.isRequired,
+  docked: React.PropTypes.bool,
+  html: React.PropTypes.array.isRequired,
   javascript: React.PropTypes.array.isRequired,
   onErrorClicked: React.PropTypes.func.isRequired,
-  docked: React.PropTypes.bool,
 };
 
 export default ErrorList;
