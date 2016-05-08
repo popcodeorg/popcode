89d10bca0c25f4c97a49aa0174646e056cc6929c
Mat Brown <mat.a.brown@gmail.com>
2016-05-08 13:56:35 -0400

    Fix lint in ErrorItem component
    First stateless component!

--- src/components/ErrorItem.jsx
+++ src/components/ErrorItem.jsx
@@ -1,32 +1,28 @@
 import React from 'react';
 import partial from 'lodash/partial';
 
-class ErrorItem extends React.Component {
-  render() {
-    const lineNumber = this.props.row + 1;
-
-    return (
-      <li
-        className="errorList-error"
-        data-error-reason={this.props.reason}
-        onClick={partial(
-          this.props.onClick,
-          this.props.row,
-          this.props.column
-        )}
-      >
-        <span className="errorList-error-line">{lineNumber}</span>
-        <span className="errorList-error-message">{this.props.text}</span>
-      </li>
-    );
-  }
+function ErrorItem(props) {
+  return (
+    <li
+      className="errorList-error"
+      data-error-reason={props.reason}
+      onClick={partial(
+        props.onClick,
+        props.row,
+        props.column
+      )}
+    >
+      <span className="errorList-error-line">{props.row + 1}</span>
+      <span className="errorList-error-message">{props.text}</span>
+    </li>
+  );
 }
 
 ErrorItem.propTypes = {
-  row: React.PropTypes.number.isRequired,
   column: React.PropTypes.number.isRequired,
-  text: React.PropTypes.string.isRequired,
   reason: React.PropTypes.string.isRequired,
+  row: React.PropTypes.number.isRequired,
+  text: React.PropTypes.string.isRequired,
   onClick: React.PropTypes.func.isRequired,
 };
 
