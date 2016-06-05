b6f6eee41a520155e4a2b52572af81ac63c04ddb
Mat Brown <mat.a.brown@gmail.com>
2016-06-05 19:20:51 -0400

    Lint for ProjectList component

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,48 +1,47 @@
 import React from 'react';
 import moment from 'moment';
 import classnames from 'classnames';
+import partial from 'lodash';
 import {generateTextPreview} from '../util/generatePreview';
 
 const MAX_LENGTH = 50;
 
-class ProjectList extends React.Component {
-  render() {
-    const projects = this.props.projects.map((project) => {
-      const preview = generateTextPreview(project);
-      const isSelected =
-        project.projectKey === this.props.currentProject.projectKey;
-
-      return (
-        <div
-          key={project.projectKey}
-          className={classnames(
-            'projectPreview',
-            'dashboard-menu-item',
-            {'dashboard-menu-item--active': isSelected}
-          )}
-          onClick={this.props.onProjectSelected.bind(null, project)}
-        >
-          <div className="projectPreview-timestamp">
-            {moment(project.updatedAt).fromNow()}
-          </div>
-          <div className="projectPreview-previewText">
-            {preview.slice(0, MAX_LENGTH)}
-          </div>
-        </div>
-      );
-    });
+function ProjectList(props) {
+  const projects = props.projects.map((project) => {
+    const preview = generateTextPreview(project);
+    const isSelected =
+      project.projectKey === props.currentProject.projectKey;
 
     return (
-      <div className="dashboard-menu dashboard-menu--scrollable">
-        {projects}
+      <div
+        className={classnames(
+          'projectPreview',
+          'dashboard-menu-item',
+          {'dashboard-menu-item--active': isSelected}
+        )}
+        key={project.projectKey}
+        onClick={partial(props.onProjectSelected, project)}
+      >
+        <div className="projectPreview-timestamp">
+          {moment(project.updatedAt).fromNow()}
+        </div>
+        <div className="projectPreview-previewText">
+          {preview.slice(0, MAX_LENGTH)}
+        </div>
       </div>
     );
-  }
+  });
+
+  return (
+    <div className="dashboard-menu dashboard-menu--scrollable">
+      {projects}
+    </div>
+  );
 }
 
 ProjectList.propTypes = {
-  projects: React.PropTypes.array.isRequired,
   currentProject: React.PropTypes.object.isRequired,
+  projects: React.PropTypes.array.isRequired,
   onProjectSelected: React.PropTypes.func.isRequired,
 };
 
