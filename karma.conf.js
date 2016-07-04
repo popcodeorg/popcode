be842c9d0570cccc801821bd28986e3704bb2408
Mat Brown <mat.a.brown@gmail.com>
2016-07-04 11:50:04 -0600

    Set up Karma for CI

--- karma.conf.js
+++ karma.conf.js
@@ -1,31 +1,22 @@
-// Karma configuration
-// Generated on Sat Jul 02 2016 17:14:56 GMT-0600 (MDT)
+/* eslint-env node */
+/* eslint-disable no-var, object-shorthand */
+
+var isCi = Boolean(process.env.TRAVIS);
+var customBrowsers = ['Chrome', 'Firefox', 'Safari', 'PhantomJS'];
+if (isCi) {
+  customBrowsers = ['Firefox', 'PhantomJS'];
+}
 
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha', 'sinon-chai', 'browserify'],
 
-
-    // list of files / patterns to load in the browser
     files: [
       'spec/examples/**/*.spec.js',
     ],
 
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'spec/examples/**/*.spec.js': ['browserify'],
     },
@@ -40,42 +31,21 @@ module.exports = function(config) {
       ],
     },
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'mocha'],
+    mochaReporter: {
+      output: isCi ? 'mocha' : 'autowatch',
+      showDiff: true,
+    },
 
+    reporters: ['mocha'],
 
-    // web server port
     port: 9876,
 
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
 
+    logLevel: config.LOG_WARN,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR ||
-    // config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    browsers: customBrowsers,
 
-
-    // enable / disable watching file and executing tests whenever any file
-    // changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox', 'Safari', 'PhantomJS'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
     concurrency: Infinity,
   });
 };
