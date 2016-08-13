7c68df1c963982620e2f4373dffa8cab5a22c529
Mat Brown <mat.a.brown@gmail.com>
2016-08-13 11:59:37 -0400

    Test for full suite of flex properties

--- spec/examples/validations/css.spec.js
+++ spec/examples/validations/css.spec.js
@@ -19,8 +19,18 @@ function assertFailsCssValidationWith(source, ...errors) {
 describe('css', () => {
   it('allows valid flexbox', () =>
     assertPassesCssValidation(`
-      .flex-container { display: flex; }
-      .flex-item { flex: 1 0 auto; }
+      .flex-container {
+        display: flex;
+        flex-flow: nowrap column;
+        align-content: flex-end;
+        justify-content: flex-start;
+        align-items: center;
+      }
+      .flex-item {
+        flex: 1 0 auto;
+        align-self: flex-end;
+        order: 2;
+      }
     `)
   );
 
