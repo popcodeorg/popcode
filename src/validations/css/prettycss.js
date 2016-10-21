fe81b7f11210f3bfe49a6386f37237d7b8e72f68
jwang1919 <jesse.wang19@gmail.com>
2016-10-20 21:22:27 -0400

    Added ^ in front of regexp. Removed initial and unset values. Added $ at end of inherit.

--- src/validations/css/prettycss.js
+++ src/validations/css/prettycss.js
@@ -8,19 +8,17 @@ const RADIAL_GRADIENT_EXPR =
 
 const FILTER_VALUE_EXPR =
   new RegExp([
-    'blur\\(',
-    'brightness\\(',
-    'contrast\\(',
-    'drop-shadow\\(',
-    'grayscale\\(',
-    'hue-rotate\\(',
-    'invert\\(',
-    'opacity\\(',
-    'saturate\\(',
-    'sepia\\(',
-    'inherit',
-    'initial',
-    'unset'].join('|'));
+    '^blur\\(',
+    '^brightness\\(',
+    '^contrast\\(',
+    '^drop-shadow\\(',
+    '^grayscale\\(',
+    '^hue-rotate\\(',
+    '^invert\\(',
+    '^opacity\\(',
+    '^saturate\\(',
+    '^sepia\\(',
+    '^inherit$'].join('|'));
 
 function isIncorrectlyRejectedValue(value) {
   return isIncorrectlyRejectedRadialGradientValue(value) ||
