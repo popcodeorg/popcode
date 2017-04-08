537152481225ff2cf6303bdc4aa03c6f71593252
Jeremy Schrader <jschrader@wintellect.com>
2017-04-07 20:32:13 -0700

    localize a vs an term

--- src/util/initI18n.js
+++ src/util/initI18n.js
@@ -1,9 +1,31 @@
 import {init} from 'i18next';
 import resources from '../../locales';
 
+const getVariationOfAOrAn = function(value, capitalize) {
+  const letters = ['a','e','i','o','u','h'];
+  let firstLetter = value.substring(0,1);
+  let correctWordForm = '';
+  if (letters.find(function(l) {
+    return firstLetter === l;
+  })) {
+    correctWordForm = capitalize ? 'An' : 'an';
+  } else {
+    correctWordForm =  capitalize ? 'A' : 'a';
+  }
+
+  return correctWordForm;
+}
+
 export default function() {
   init({
     fallbackLng: 'en',
     resources,
+    interpolation: {
+      format: function(value, format, lng) {
+        if (format === 'en-handle-an') return (!lng || lng === 'en') ? getVariationOfAOrAn(value, false) : '';
+        if (format === 'en-handle-an-capitalized') return (!lng || lng === 'en') ? getVariationAOrAn(value, true) : '';
+        return value;
+      }
+    }
   });
 }
