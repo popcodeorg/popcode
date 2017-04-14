85a9be0a23af4ea7c1e7b9da65100cfe73da20c2
Jeremy Schrader <jschrader@wintellect.com>
2017-04-14 11:41:08 -0700

    fix lint errors

--- src/util/initI18n.js
+++ src/util/initI18n.js
@@ -1,27 +1,36 @@
 import {init} from 'i18next';
 import resources from '../../locales';
 
-// value:   The value of the variable that is being passed in to be interpolated into the localized string
-// format:  the pipe-delimited list of formatting flags, it will run the formatting functions sequentially
-//          based on the sequence of the flags passed in.
-// lng:     the language used for the formatting 
-const format = (value, format, lng) => {
-  const formatFlags = format.split('|');    
-  formatFlags.forEach(flag => {
-    // replace the value of the input value with our new value("a" or "an")
-    if (flag === 'en-handle-an') {
-      value = getVariationOfAOrAn(value, false);
-    }
-    else if (flag === 'capitalize') {
-      value = value.charAt(0).toUpperCase() + value.slice(1);
-    } 
-  })
-  return value;
+function getVariationOfAOrAn(value) {
+  const vowelishLetters = ['a', 'e', 'i', 'o', 'u', 'h'];
+  const isVowelish = vowelishLetters.find(l => value.substring(0, 1) === l);
+  return isVowelish ? 'an' : 'a';
 }
 
-const getVariationOfAOrAn = function(value, capitalize) {
-  const isVowelish = ['a','e','i','o','u','h'].find((l) => {return value.substring(0,1) === l;}) 
-  return isVowelish ? 'an' : 'a';
+/*
+Arguments:
+value:   The value of the variable that is being passed
+  in to be interpolated into the localized string
+format:  the pipe-delimited list of formatting flags,
+  it will run the formatting functions sequentially
+  based on the sequence of the flags passed in.
+lng: the language used for the formatting
+NOTE: lng is available but is not currently used, so I've removed
+it from the arguments list.
+*/
+
+function formatFunc(value, format) {
+  let newVal = value;
+  const formatFlags = format.split('|');
+  formatFlags.forEach((flag) => {
+    // replace the value of the input value with our new value("a" or "an")
+    if (flag === 'en-handle-an') {
+      newVal = getVariationOfAOrAn(newVal, false);
+    } else if (flag === 'capitalize') {
+      newVal = newVal.charAt(0).toUpperCase() + newVal.slice(1);
+    }
+  });
+  return newVal;
 }
 
 export default function() {
@@ -29,7 +38,7 @@ export default function() {
     fallbackLng: 'en',
     resources,
     interpolation: {
-      format: format
-    }
+      format: formatFunc,
+    },
   });
 }
