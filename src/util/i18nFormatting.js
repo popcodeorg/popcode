b09ca78aed837a7c549cf6853aa5eb492bee1cb3
Jeremy Schrader <jschrader@wintellect.com>
2017-04-20 22:32:32 -0700

    add constants map for format flags

--- src/util/i18nFormatting.js
+++ src/util/i18nFormatting.js
@@ -1,3 +1,8 @@
+const formatFlags = {
+  EN_HANDLE_AN: 'en-handle-an',
+  CAPITALIZE: 'capitalize'
+}
+
 function getVariationOfAOrAn(value) {
   const vowelishLetters = ['a', 'e', 'i', 'o', 'u', 'h'];
   const isVowelish = vowelishLetters.find(l => value.substring(0, 1) === l);
@@ -7,11 +12,11 @@ function getVariationOfAOrAn(value) {
 // eslint-disable-next-line no-unused-vars
 export default function(value, format, lng) {
   let newVal = value;
-  const formatFlags = format.split('|');
-  formatFlags.forEach((flag) => {
-    if (flag === 'en-handle-an') {
+  const inputFlags = format.split('|');
+  inputFlags.forEach((flag) => {
+    if (flag === formatFlags.EN_HANDLE_AN) {
       newVal = getVariationOfAOrAn(newVal, false);
-    } else if (flag === 'capitalize') {
+    } else if (flag === formatFlags.CAPITALIZE) {
       newVal = newVal.charAt(0).toUpperCase() + newVal.slice(1);
     }
   });
