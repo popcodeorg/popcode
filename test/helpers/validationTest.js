0a6da81a6ce95afc7f888658942120346534ccc5
Mat Brown <mat@genius.com>
2017-05-29 21:42:00 -0400

    Convert HTML validation specs to tape tests
    Ports all HTML validation specs to tape tests. Also introduces the
    `validationTest` helper, which provides a macro for Tape tests that
    takes an input string, a validator, and a spread of expected errors
    (objects with properties `reason`, `row`, and optionally `payload`).

---
+++ test/helpers/validationTest.js
@@ -0,0 +1,21 @@
+import map from 'lodash/map';
+import orderBy from 'lodash/orderBy';
+import pick from 'lodash/pick';
+
+export default function validationTest(input, validate, ...expectedErrors) {
+  return async (assert) => {
+    try {
+      const errors = await validate(input);
+      assert.deepEqual(
+        map(
+          orderBy(errors, ['reason', 'row']),
+          error => pick(error, ['reason', 'row', 'payload']),
+        ),
+        orderBy(expectedErrors, ['reason', 'row']),
+      );
+    } catch (e) {
+      assert.error(e);
+    }
+    assert.end();
+  };
+}
