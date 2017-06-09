2027074e892a784c52bd90b82a4e842b050f56ed
Mat Brown <mat@genius.com>
2017-06-09 17:42:03 -0400

    Error, ErrorList, ErrorReport records

---
+++ src/records/ErrorList.js
@@ -0,0 +1,6 @@
+import {List, Record} from 'immutable';
+
+export default Record({
+  items: new List(),
+  state: 'passed',
+}, 'ErrorList');
--- src/records/index.js
+++ src/records/index.js
@@ -1,5 +1,11 @@
+import Error from './Error';
+import ErrorList from './ErrorList';
+import ErrorReport from './ErrorReport';
 import Project from './Project';
 
 export {
+  Error,
+  ErrorList,
+  ErrorReport,
   Project,
 };
