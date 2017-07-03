2e5bfbf6d53e343241cf7570a897f594a055bae7
Mat Brown <mat@genius.com>
2017-07-03 18:25:59 -0400

    Remove logging

--- src/application.js
+++ src/application.js
@@ -1,33 +1,19 @@
 import './init';
-import './util/Bugsnag';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Immutable from 'immutable';
 import installDevTools from 'immutable-devtools';
+import {install as installOfflinePlugin} from 'offline-plugin/runtime';
+import Bugsnag from './util/Bugsnag';
 import Application from './components/Application';
 import initI18n from './util/initI18n';
-import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 import {init as initAnalytics, logPageview} from './clients/googleAnalytics';
 
 installDevTools(Immutable);
-OfflinePluginRuntime.install({
-  onUpdating: () => {
-    console.log('SW Event:', 'onUpdating');
+installOfflinePlugin({
+  onUpdateFailed() {
+    Bugsnag.notify('ServiceWorker update failed');
   },
-  onUpdateReady: () => {
-    console.log('SW Event:', 'onUpdateReady');
-    // Tells to new SW to take control immediately
-    runtime.applyUpdate();
-  },
-  onUpdated: () => {
-    console.log('SW Event:', 'onUpdated');
-    // Reload the webpage to load into the new version
-    window.location.reload();
-  },
-
-  onUpdateFailed: () => {
-    console.log('SW Event:', 'onUpdateFailed');
-  }
 });
 
 initI18n();
