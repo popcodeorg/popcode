d1f33ff308512cc4f6110fb42bfb503e2aa85c79
Mat Brown <mat@genius.com>
2017-08-13 13:09:53 -0400

    Don’t assume non-null current project when selecting enabled libraries
    If there is no current project, there are no enabled libraries.

--- src/selectors/getEnabledLibraries.js
+++ src/selectors/getEnabledLibraries.js
@@ -1,7 +1,8 @@
 import {createSelector} from 'reselect';
+import get from 'lodash/get';
 import getCurrentProject from './getCurrentProject';
 
 export default createSelector(
   [getCurrentProject],
-  currentProject => currentProject.enabledLibraries,
+  currentProject => get(currentProject, ['enabledLibraries'], []),
 );
