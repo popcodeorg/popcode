2883f4bd5cba411e8c36544d869fa28bed8c7af6
Mat Brown <mat@genius.com>
2017-11-03 18:11:37 -0400

    Revert Bugsnag bespoke error grouping code
    This was premised on the idea that we know the sourcemapped backtrace of
    the error in the application environment, which is of course entirely
    incorrect (unless dev tools happen to be open), because the sourcemaps
    aren’t loaded.

--- src/util/Bugsnag.js
+++ src/util/Bugsnag.js
@@ -1,32 +1,15 @@
 import 'bugsnag-js';
-import isNull from 'lodash/isNull';
 import config from '../config';
 import {getCurrentProject} from '../selectors';
-
-const MINIFIED_SOURCE_PATTERN = /firebase\/auth/;
 
 const Bugsnag = window.Bugsnag.noConflict();
 Bugsnag.apiKey = config.bugsnagApiKey;
 Bugsnag.releaseStage = config.nodeEnv;
 Bugsnag.appVersion = config.gitRevision;
-Bugsnag.enableNotifyUnhandledRejections();
 
-const bugsnagNotifyMiddleware = {
-  store: null,
-
-  beforeNotify(payload) {
-    if (MINIFIED_SOURCE_PATTERN.test(payload.stacktrace)) {
-      payload.metaData.groupingHash = payload.message;
-    }
-    this._attachStateToPayload(payload);
-  },
-
-  _attachStateToPayload(payload) {
-    if (isNull(this.store)) {
-      return;
-    }
-
-    const state = this.store.getState();
+export function includeStoreInBugReports(store) {
+  Bugsnag.beforeNotify = (payload) => {
+    const state = store.getState();
     if (state.get('user')) {
       payload.user = state.get('user').toJS();
     } else {
@@ -37,14 +20,9 @@ const bugsnagNotifyMiddleware = {
     if (currentProject) {
       payload.metaData.currentProject = currentProject;
     }
-  },
-};
-
-Bugsnag.beforeNotify =
-  bugsnagNotifyMiddleware.beforeNotify.bind(bugsnagNotifyMiddleware);
-
-export function includeStoreInBugReports(store) {
-  bugsnagNotifyMiddleware.store = store;
+  };
 }
+
+Bugsnag.enableNotifyUnhandledRejections();
 
 export default Bugsnag;
