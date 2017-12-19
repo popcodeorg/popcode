e20f5c8112908d17865fea48d2b23ac069a12f96
Mat Brown <mat.a.brown@gmail.com>
2017-12-18 21:39:38 -0500

    Revert "Revert "Load preview libraries async""

--- src/validations/css/stylelint.js
+++ src/validations/css/stylelint.js
@@ -1,5 +1,5 @@
 import Validator from '../Validator';
-import retryingFailedImports from '../retryingFailedImports';
+import retryingFailedImports from '../../util/retryingFailedImports';
 
 const errorMap = {
   'syntaxError/Unclosed block': () => ({
