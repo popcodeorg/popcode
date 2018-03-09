54487ad46d0029f6f553c12a5f955d4b7cd9bf5d
Aaron Greenberg <p@aaronjgreenberg.com>
2018-03-09 18:05:20 +0000

    Use i18n functionality for button text

--- src/components/InstructionsEditor.jsx
+++ src/components/InstructionsEditor.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {t} from 'i18next';
 
 export default function InstructionsEditor({instructions, onCancelEditing}) {
   return (
     <div className="instructions-editor">
       <div className="instructions-editor-menu">
-        <button onClick={onCancelEditing}>Cancel</button>
+        <button onClick={onCancelEditing}>
+          {t('workspace.components.instructions.cancel')}
+        </button>
       </div>
       <pre contentEditable>
         {instructions}
