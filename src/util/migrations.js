5b570dd1cd067485468bb30849c38dcad0adf802
Yining Chen <yining.chen@nyu.edu>
2018-03-21 17:25:52 -0400

    remove commented out code

--- src/util/migrations.js
+++ src/util/migrations.js
@@ -1,41 +1,6 @@
 import isArray from 'lodash/isArray';
 import isPlainObject from 'lodash/isPlainObject';
 
-/*
-export function migrateToMap(collection, transformer) {
-  if (Map.isMap(collection)) {
-    return collection;
-  } else if (!isCollection(collection)) {
-    return new Map();
-  } else if (!isFunction(transformer)) {
-    return collection.toMap();
-  }
-
-  const map = {};
-  for (const key of collection.keys()) {
-    map[key] = transformer(key);
-  }
-  return fromJS(map);
-}
-
-export function migrateHidden(componentName) {
-  const languageMatch = componentName.match(/editor\.(\w+)/);
-  const language = languageMatch && languageMatch[1];
-  return new HiddenUIComponent({componentName, language});
-}
-
-export function createHiddenComponent(componentName) {
-  return new HiddenUIComponent({componentName});
-}
-
-export function migrateHiddenUIComponents(hiddenUIComponents) {
-  return migrateToMap(
-    hiddenUIComponents,
-    createHiddenComponent,
-  ).set('console', createHiddenComponent('console'));
-}
-*/
-
 export function hiddenUIArrayToObject(hiddenUIComponents) {
   let obj = {};
   if (isPlainObject(hiddenUIComponents)) {
