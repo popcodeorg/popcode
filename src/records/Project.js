2fe7483d2f57c09334e27c934134c94ccb9a9300
Yining Chen <yining.chen@nyu.edu>
2018-03-25 19:02:21 -0400

    move migration function into records

--- src/records/Project.js
+++ src/records/Project.js
@@ -1,8 +1,25 @@
 import {Record, Set, Map} from 'immutable';
+import isArray from 'lodash/isArray';
+import isPlainObject from 'lodash/isPlainObject';
 
 import HTML_TEMPLATE from '../../templates/new.html';
-import {hiddenUIArrayToObject} from '../util/migrations';
 import HiddenUIComponent from './HiddenUIComponent';
+
+function hiddenUIArrayToObject(hiddenUIComponents) {
+  let obj = {};
+  if (isPlainObject(hiddenUIComponents)) {
+    obj = hiddenUIComponents;
+  } else if (isArray(hiddenUIComponents)) {
+    let component;
+    for (component of hiddenUIComponents) {
+      const [componentType, language] = component.split('.');
+      const componentKey = language || componentType;
+      obj[componentKey] = new HiddenUIComponent({componentType, language});
+    }
+  }
+  obj.console = new HiddenUIComponent({componentType: 'console'});
+  return obj;
+}
 
 const Sources = Record({
   html: HTML_TEMPLATE,
