19a5f331c50c3c6075d39923821941e70cf39e5c
Matt Garbis <mgarbis@gmail.com>
2018-04-24 21:45:32 -0400

    add react-coroutine, async load InstructionsEditor and fade in

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {toReact as markdownToReact} from '../util/markdown';
-import InstructionsEditor from './InstructionsEditor';
+import InstructionsEditor from './InstructionsEditorAsync';
 
 export default function Instructions({
   instructions,
