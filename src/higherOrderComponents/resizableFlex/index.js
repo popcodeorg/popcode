a589c7a6cdb78fd2ff9a9c48ee595b4b8ebb3701
Mat Brown <mat.a.brown@gmail.com>
2018-06-02 19:56:24 -0400

    Efficient prop updates for resizable flex higher-order component
    Ensures we don’t propagate prop updates from the resizable flex HOC
    unless something relevant has actually changed.
    
    The main functions in the `resizableFlex` module are independent of the
    current state, so we should just define them once and then merge them
    into the final props returned by the component.
    
    The props-returning function should return the same instance as
    previously if the props themselves are unchanged. To do this, we want to
    memoize the function so that new props are only returned if:
    
    * The flex list (received from Redux) has changed
    * The `ownProps` (passed directly to the component) have changed
    
    In the latter case, we can’t rely on `ownProps` to be strictly equal,
    however; instead, we need to create a custom memoized function that does
    a shallow value check (the same kind of check used for pure
    `react-redux` containers)

--- src/higherOrderComponents/resizableFlex/index.js
+++ src/higherOrderComponents/resizableFlex/index.js
@@ -4,7 +4,9 @@ import findIndex from 'lodash-es/findIndex';
 import isNull from 'lodash-es/isNull';
 import map from 'lodash-es/map';
 import merge from 'lodash-es/merge';
+import shallowequal from 'shallowequal';
 import times from 'lodash-es/times';
+import {createSelector, defaultMemoize} from 'reselect';
 
 import {makeGetResizableFlexGrow} from '../../selectors';
 import {updateResizableFlex} from '../../actions';
@@ -23,67 +25,76 @@ export default function resizableFlex(size) {
       const regions = times(size, () => ({current: null}));
       const initialMainSizes = times(size, () => null);
 
-      return (state, ownProps) => merge(
-        {
-          onResizableFlexDividerDrag(beforeIndex, event, payload) {
-            const afterIndex = findIndex(regions, 'current', beforeIndex + 1);
-            const [{current: before}, {current: after}] =
-              at(regions, [beforeIndex, afterIndex]);
+      const stateIndependentFunctions = {
+        onResizableFlexDividerDrag(beforeIndex, event, payload) {
+          const afterIndex = findIndex(regions, 'current', beforeIndex + 1);
+          const [{current: before}, {current: after}] =
+            at(regions, [beforeIndex, afterIndex]);
 
-            const {getCurrentSize, getDesiredSize} =
-              directionAdapterFor(before);
+          const {getCurrentSize, getDesiredSize} =
+            directionAdapterFor(before);
 
-            const [desiredBeforeFlexGrow, desiredAfterFlexGrow] =
-              calculateFlexGrowAfterDrag(
-                {
-                  currentFlexGrow: Number(
-                    getComputedStyle(before)['flex-grow'],
-                  ),
-                  currentSize: getCurrentSize(before),
-                  desiredSize: getDesiredSize(before, payload),
-                  initialMainSize: initialMainSizes[beforeIndex],
-                },
-                {
-                  currentFlexGrow: Number(
-                    getComputedStyle(after)['flex-grow'],
-                  ),
-                  currentSize: getCurrentSize(after),
-                  initialMainSize: initialMainSizes[afterIndex],
-                },
-              );
+          const [desiredBeforeFlexGrow, desiredAfterFlexGrow] =
+            calculateFlexGrowAfterDrag(
+              {
+                currentFlexGrow: Number(
+                  getComputedStyle(before)['flex-grow'],
+                ),
+                currentSize: getCurrentSize(before),
+                desiredSize: getDesiredSize(before, payload),
+                initialMainSize: initialMainSizes[beforeIndex],
+              },
+              {
+                currentFlexGrow: Number(
+                  getComputedStyle(after)['flex-grow'],
+                ),
+                currentSize: getCurrentSize(after),
+                initialMainSize: initialMainSizes[afterIndex],
+              },
+            );
 
-            dispatch(updateResizableFlex(
-              instanceId,
-              [
-                {index: beforeIndex, flexGrow: desiredBeforeFlexGrow},
-                {index: afterIndex, flexGrow: desiredAfterFlexGrow},
-              ],
-            ));
-          },
-
-          resizableFlexRefs: map(
-            regions,
-            (region, index) => (element) => {
-              region.current = element;
-              if (isNull(element)) {
-                initialMainSizes[index] = null;
-                return;
-              }
-
-              const flexGrowWas = element.style.flexGrow;
-              const flexShrinkWas = element.style.flexShrink;
-              element.style.flexGrow = element.style.flexShrink = '0';
-              initialMainSizes[index] = directionAdapterFor(element).
-                getCurrentSize(element);
-              element.style.flexGrow = flexGrowWas;
-              element.style.flexShrink = flexShrinkWas;
-            },
-          ),
-
-          resizableFlexGrow: getResizableFlexGrow(state),
+          dispatch(updateResizableFlex(
+            instanceId,
+            [
+              {index: beforeIndex, flexGrow: desiredBeforeFlexGrow},
+              {index: afterIndex, flexGrow: desiredAfterFlexGrow},
+            ],
+          ));
         },
 
-        ownProps,
+        resizableFlexRefs: map(
+          regions,
+          (region, index) => (element) => {
+            region.current = element;
+            if (isNull(element)) {
+              initialMainSizes[index] = null;
+              return;
+            }
+
+            const flexGrowWas = element.style.flexGrow;
+            const flexShrinkWas = element.style.flexShrink;
+            element.style.flexGrow = element.style.flexShrink = '0';
+            initialMainSizes[index] = directionAdapterFor(element).
+              getCurrentSize(element);
+            element.style.flexGrow = flexGrowWas;
+            element.style.flexShrink = flexShrinkWas;
+          },
+        ),
+      };
+
+      return createSelector(
+        [
+          getResizableFlexGrow,
+          defaultMemoize(
+            (_state, ownProps) => ownProps,
+            shallowequal,
+          ),
+        ],
+        (resizableFlexGrow, ownProps) => merge(
+          {resizableFlexGrow},
+          ownProps,
+          stateIndependentFunctions,
+        ),
       );
     },
     {
