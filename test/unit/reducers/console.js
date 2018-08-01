2aa7b13d03376b0958be934d48c9a2e5fe96968f
Razzi Abuissa <razzi53@gmail.com>
2018-08-01 16:51:36 -0700

    Update and add console reducer tests

--- test/unit/reducers/console.js
+++ test/unit/reducers/console.js
@@ -8,35 +8,57 @@ import {
   consoleLogProduced,
   consoleValueProduced,
   evaluateConsoleEntry,
+  setPreviousHistoryIndex,
+  setCurrentConsoleInput,
 } from '../../../src/actions';
 import reducerTest from '../../helpers/reducerTest';
 import reducer from '../../../src/reducers/console';
 
-const initialState = new OrderedMap();
+const initialState = {
+  history: new OrderedMap(),
+  previousHistoryIndex: 0,
+  previousConsoleInput: '',
+};
 
 test('evaluateConsoleEntry', reducerTest(
   reducer,
   initialState,
   partial(evaluateConsoleEntry, '1+1', '123'),
-  new OrderedMap({123: new ConsoleEntry({expression: '1+1'})}),
+  {
+    previousHistoryIndex: 0,
+    previousConsoleInput: '',
+    history: new OrderedMap({123: new ConsoleEntry({expression: '1+1'})}),
+  },
 ));
 
 test('consoleValueProduced', reducerTest(
   reducer,
-  new OrderedMap({123: new ConsoleEntry({expression: '1+1'})}),
+  {
+    previousHistoryIndex: 0,
+    previousConsoleInput: '',
+    history: new OrderedMap({123: new ConsoleEntry({expression: '1+1'})}),
+  },
   partial(consoleValueProduced, '123', 2, 123456789),
-  new OrderedMap({
-    123: new ConsoleEntry({
-      expression: '1+1',
-      value: 2,
-      evaluatedByCompiledProjectKey: 123456789,
+  {
+    history: new OrderedMap({
+      123: new ConsoleEntry({
+        expression: '1+1',
+        value: 2,
+        evaluatedByCompiledProjectKey: 123456789,
+      }),
     }),
-  }),
+  },
 ));
 
 test('consoleErrorProduced', reducerTest(
   reducer,
-  new OrderedMap({123: new ConsoleEntry({expression: 'bogus + 1'})}),
+  {
+    previousHistoryIndex: 0,
+    previousConsoleInput: '',
+    history: new OrderedMap({
+      123: new ConsoleEntry({expression: 'bogus + 1'}),
+    }),
+  },
   partial(
     consoleErrorProduced,
     '123',
@@ -44,26 +66,56 @@ test('consoleErrorProduced', reducerTest(
     'bogus is not defined',
     123456789,
   ),
-  new OrderedMap({
-    123: new ConsoleEntry({
-      expression: 'bogus + 1',
-      error: new ConsoleError({
-        name: 'NameError',
-        message: 'bogus is not defined',
+  {
+    previousHistoryIndex: 0,
+    previousConsoleInput: '',
+    history: new OrderedMap({
+      123: new ConsoleEntry({
+        expression: 'bogus + 1',
+        error: new ConsoleError({
+          name: 'NameError',
+          message: 'bogus is not defined',
+        }),
+        evaluatedByCompiledProjectKey: 123456789,
       }),
-      evaluatedByCompiledProjectKey: 123456789,
     }),
-  }),
+  },
 ));
 
 test('consoleLogProduced', reducerTest(
   reducer,
   initialState,
   partial(consoleLogProduced, 'A console message', 123456789, '456'),
-  new OrderedMap({
-    456: new ConsoleEntry({
-      value: 'A console message',
-      evaluatedByCompiledProjectKey: 123456789,
+  {
+    previousHistoryIndex: 0,
+    previousConsoleInput: '',
+    history: new OrderedMap({
+      456: new ConsoleEntry({
+        value: 'A console message',
+        evaluatedByCompiledProjectKey: 123456789,
+      }),
     }),
-  }),
+  },
+));
+
+test('setPreviousHistoryIndex', reducerTest(
+  reducer,
+  initialState,
+  partial(setPreviousHistoryIndex, 1),
+  {
+    previousHistoryIndex: 1,
+    previousConsoleInput: '',
+    history: new OrderedMap(),
+  },
+));
+
+test('setCurrentConsoleInput', reducerTest(
+  reducer,
+  initialState,
+  partial(setCurrentConsoleInput, '3'),
+  {
+    previousHistoryIndex: 0,
+    previousConsoleInput: '3',
+    history: new OrderedMap(),
+  },
 ));
