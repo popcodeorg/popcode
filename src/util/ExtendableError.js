b9b2cfdd4c10e3d890ffb2267f990b3b05d00171
Mat Brown <mat.a.brown@gmail.com>
2018-08-10 13:05:50 -0400

    Retries and error handling for gapi loading
    We may fail to load the `gapi` SDK, for instance because of bad network
    conditions. So:
    
    * Retry a bunch of times, like we do for everything
    * Reject the promise if we still fail to load after all those retries
    
    Currently there’s no upstream handling for the promise rejection but if
    it still appears to be an issue in production after all the retries, we
    can add that.

---
+++ src/util/ExtendableError.js
@@ -0,0 +1,8 @@
+export default function ExtendableError(message) {
+  this.name = this.constructor.name;
+  this.message = message;
+  this.stack = (new Error(message)).stack;
+}
+
+ExtendableError.prototype = Object.create(Error.prototype);
+ExtendableError.prototype.constructor = ExtendableError;
