c7dcbaf0cf0b6bed20645bebae991294fa76411e
Wylie Conlon <wylieconlon@gmail.com>
2018-10-22 15:25:44 -0400

    Add highlighting for some jQuery selectors

--- src/util/selectorAtCursor.js
+++ src/util/selectorAtCursor.js
@@ -1,3 +1,6 @@
+import {parse} from '@babel/parser';
+import traverse from '@babel/traverse';
+import {isStringLiteral} from '@babel/types';
 import postcss from 'postcss';
 
 export function selectorAtCursor(source, cursor, language) {
@@ -22,6 +25,33 @@ export function selectorAtCursor(source, cursor, language) {
         }
         return null;
       });
+      return highlighterSelector;
+    } catch (e) {
+      return null;
+    }
+  } else if (language === 'javascript') {
+    try {
+      const ast = parse(source);
+
+      const visitor = {
+        CallExpression(path) {
+          // Only matches code of the form $('selector') on the same line
+          const expressionLoc = path.node.loc;
+          const cursorLine = cursor.row + 1;
+          const [selector] = path.node.arguments;
+          if (
+            path.get('callee').toString() === '$' &&
+            isStringLiteral(selector) &&
+            expressionLoc.start.line >= cursorLine &&
+            expressionLoc.end.line <= cursorLine
+          ) {
+            highlighterSelector = selector.value;
+            path.stop();
+          }
+        },
+      };
+      traverse(ast, visitor, null);
+
       return highlighterSelector;
     } catch (e) {
       return null;
--- test/unit/util/selectorAtCursor.js
+++ test/unit/util/selectorAtCursor.js
@@ -1,20 +1,42 @@
 import test from 'tape';
 
 import {selectorAtCursor} from '../../../src/util/selectorAtCursor';
-import {css} from '../../data/acceptance.json';
+import {css, javascript} from '../../data/acceptance.json';
 
-const [source] = css;
+test('css rules', (t) => {
+  const [source] = css;
 
-test('cursor in css rule returns correct selector', (assert) => {
-  const cursor = {column: 1, row: 0};
-  assert.isEqual(selectorAtCursor(source, cursor, 'css'), 'body');
-  assert.end();
+  t.test('cursor in css rule returns correct selector', (assert) => {
+    const cursor = {column: 1, row: 0};
+    assert.isEqual(selectorAtCursor(source, cursor, 'css'), 'body');
+    assert.end();
+  });
+
+  t.test('cursor outside css rule returns null', (assert) => {
+    const cursor = {column: 1, row: 2};
+    assert.isEqual(selectorAtCursor(source, cursor, 'css'), null);
+    assert.end();
+  });
 });
 
-test('cursor outside css rule returns null', (assert) => {
-  const cursor = {column: 1, row: 2};
-  assert.isEqual(selectorAtCursor(source, cursor, 'css'), null);
-  assert.end();
+test('jquery selectors that can be analyzed', (t) => {
+  const [source] = javascript;
+
+  t.test('cursor in jquery rule with string return selector', (assert) => {
+    const cursor = {column: 5, row: 1};
+    assert.isEqual(
+      selectorAtCursor(source, cursor, 'javascript'),
+      '#submit-name',
+    );
+    assert.end();
+  });
+
+  t.test('cursor outside jquery rule with string returns null', (assert) => {
+    const cursor = {column: 2, row: 0};
+    assert.isEqual(
+      selectorAtCursor(source, cursor, 'javascript'),
+      null,
+    );
+    assert.end();
+  });
 });
-
-
