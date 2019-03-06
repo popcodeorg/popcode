01116f1e2382f8ba087b217235cdd9eed5db1bcb
Jesse Wang <jesse.wang19@gmail.com>
2019-03-05 20:27:11 -0500

    Fix unnecessary addition of an eslint-disable directive

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 /* eslint-env node */
 /* eslint-disable import/no-commonjs */
-/* eslint-disable import/no-nodejs-modules */
 
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
