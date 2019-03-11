32cc3d3fd19e4bd76ac23a85167a3fba7062dcd6
Mat Brown <mat.a.brown@gmail.com>
2019-03-11 14:03:47 -0400

    Mocks for bugsnag and firebase libraries
    These libraries are set up in code that runs at module load time, which
    is probably not a great practice, but for now we just mock out the
    libraries themselves so our modules will load without error.

---
+++ __mocks__/@firebase/app.js
@@ -0,0 +1,17 @@
+import constant from 'lodash-es/constant';
+
+class AuthProvider {
+  addScope() {}
+}
+
+export const firebase = {
+  auth: Object.assign(
+    () => ({}),
+    {
+      GithubAuthProvider: AuthProvider,
+      GoogleAuthProvider: AuthProvider,
+    },
+  ),
+
+  initializeApp: constant({}),
+};
