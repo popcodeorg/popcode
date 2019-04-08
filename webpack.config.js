4fe1c3be88a567811e70565bee1f8da07d67f373
Wylie Conlon <wylieconlon@gmail.com>
2019-04-07 20:35:25 -0400

    Run all node_modules through babel (#1674)
    * Run all node_modules through babel
    
    * Fix lint error in webpack.config.js
    
    * Respond to review feedback

--- webpack.config.js
+++ webpack.config.js
@@ -252,18 +252,7 @@ module.exports = (env = process.env.NODE_ENV || 'development') => {
         },
         {
           test: /\.js$/u,
-          include: [
-            matchModule('ansi-styles'),
-            matchModule('ast-types'),
-            matchModule('chalk'),
-            matchModule('lodash-es'),
-            matchModule('parse5'),
-            matchModule('parse5-sax-parser'),
-            matchModule('postcss-html'),
-            matchModule('recast'),
-            matchModule('redux'),
-            matchModule('stylelint'),
-          ],
+          include: [path.resolve(__dirname, 'node_modules')],
           use: {loader: 'babel-loader', options: babelLoaderConfig},
         },
         {
