27653d11497cd0939f15811f88eedd564cef01cc
Peter Jablonski <pwjablonski@gmail.com>
2019-04-15 21:17:56 -0400

    Fix project previews (#1684)
    * update package lock
    
    * delete package lock
    
    * fix project title preview
    
    * update order of selectors

--- src/containers/ProjectPreview.js
+++ src/containers/ProjectPreview.js
@@ -3,15 +3,17 @@ import {connect} from 'react-redux';
 import ProjectPreview from '../components/ProjectPreview';
 import {changeCurrentProject} from '../actions';
 import {
+  makeGetProjectPreview,
   getCurrentProjectKey,
-  getCurrentProjectPreview,
   getProject,
 } from '../selectors';
 
 function makeMapStateToProps() {
+  const getProjectPreview = makeGetProjectPreview();
+
   return function mapStateToProps(state, {projectKey}) {
     return {
-      preview: getCurrentProjectPreview(state),
+      preview: getProjectPreview(state, {projectKey}),
       isSelected: projectKey === getCurrentProjectKey(state),
       project: getProject(state, {projectKey}),
     };
