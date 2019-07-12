24f9c3e10c79fb1275e7e56213e4cb5a98142914
Razzi Abuissa <razzi53@gmail.com>
2019-07-11 19:41:40 -0700

    Swap out moment for date-fns

--- src/util/timeFormatter.js
+++ src/util/timeFormatter.js
@@ -1,8 +1,8 @@
-import moment from 'moment';
+import {format} from 'date-fns';
 
 export function dateToString(date) {
   if (date) {
-    return moment(date).format('MMM Do [at] h:mm A');
+    return format(date).format('MMM Do [at] h:mm A');
   }
   return null;
 }
