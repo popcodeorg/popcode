f2d35fd6307cd90fe879369e5e3cc8a11c2ab925
Mat Brown <mat.a.brown@gmail.com>
2019-07-15 14:17:42 -0400

    Revert "Updates CurrentUserMenu with icons (#1712)"
    This reverts commit 4e02ef0de2b925cea412501a5fd45876f8d54116.

--- src/components/TopBar/CurrentUserMenu.jsx
+++ src/components/TopBar/CurrentUserMenu.jsx
@@ -3,9 +3,6 @@ import isUndefined from 'lodash-es/isUndefined';
 import PropTypes from 'prop-types';
 import React, {Fragment} from 'react';
 import {t} from 'i18next';
-import {faGithub} from '@fortawesome/free-brands-svg-icons';
-import {faUnlink, faSignOutAlt} from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 import {UserAccount} from '../../records';
 
@@ -30,36 +27,17 @@ const CurrentUserMenu = createMenu({
       <Fragment>
         {isUndefined(githubIdentityProvider) ? (
           <MenuItem onClick={onLinkGitHub}>
-            <div className="top-bar__menu-item_container">
-              {t('top-bar.session.link-github')}
-              <div className="top-bar__menu-item-icon">
-                <FontAwesomeIcon icon={faGithub} />
-              </div>
-            </div>
+            {t('top-bar.session.link-github')}
           </MenuItem>
         ) : (
           <MenuItem onClick={onUnlinkGitHub}>
-            <div className="top-bar__menu-item_container">
-              <img
-                className="top-bar__avatar top-bar__menu-item_avatar"
-                src={githubIdentityProvider.avatarUrl}
-              />
-              {t('top-bar.session.unlink-github', {
-                displayName: githubIdentityProvider.displayName.split(' ')[0],
-              })}
-              <div className="top-bar__menu-item-icon">
-                <FontAwesomeIcon icon={faUnlink} />
-              </div>
-            </div>
+            {t('top-bar.session.unlink-github', {
+              displayName: githubIdentityProvider.displayName,
+            })}
           </MenuItem>
         )}
         <MenuItem onClick={onLogOut}>
-          <div className="top-bar__menu-item_container">
-            {t('top-bar.session.log-out-prompt')}
-            <div className="top-bar__menu-item-icon">
-              <FontAwesomeIcon icon={faSignOutAlt} />
-            </div>
-          </div>
+          {t('top-bar.session.log-out-prompt')}
         </MenuItem>
       </Fragment>
     );
