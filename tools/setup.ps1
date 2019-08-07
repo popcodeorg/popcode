e3235d3b06a2f3ddc9c2bab5f54f3aa2bfa0512c
Mat Brown <mat.a.brown@gmail.com>
2019-08-06 20:52:15 -0400

    Powershell 5 compatibility

--- tools/setup.ps1
+++ tools/setup.ps1
@@ -1,3 +1,5 @@
+#!/usr/bin/env pwsh
+
 $PathSeparator=[IO.Path]::PathSeparator
 
 $NodeVersion=(Get-Content .node-version | Select-String -Pattern '\d+\.\d+\.\d+' | ForEach-Object {$_.Matches.Value})
@@ -19,7 +21,7 @@ $NodeenvDir="nodeenv"
 
 python $(Join-Path "$NodeenvPackageDir" nodeenv.py) "--node=$NodeVersion" "$NodeenvDir"
 
-$env:PATH="$(Join-Path $(Get-Location) $NodeenvDir bin)${PathSeparator}$env:PATH"
+$env:PATH="$(Join-Path "$(Get-Location)" $(Join-Path $NodeenvDir bin))${PathSeparator}$env:PATH"
 
 $YarnVersion=$(node -e 'console.log(JSON.parse(fs.readFileSync(\"package.json\")).engines.yarn)' | Select-String -Pattern '\d+\.\d+\.\d+' | ForEach-Object {$_.Matches.Value})
 
