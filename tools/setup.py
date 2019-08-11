7031d34498f198b8e96d0737666c4e8a7f45bf72
Mat Brown <mat.a.brown@gmail.com>
2019-08-10 21:11:30 -0400

    Port all NTDE tools scripts to Python

--- tools/setup.py
+++ tools/setup.py
@@ -2,7 +2,7 @@
 
 from io import BytesIO
 import json
-from os import path
+import os
 import re
 import shutil
 import subprocess
@@ -15,27 +15,31 @@ try:
 except ImportError:
     from urllib2 import urlopen
 
-from util import (NODEENV_DIR, run_in_nodeenv)
+from util import (
+    NODEENV_DIR,
+    POPCODE_ROOT,
+    run_in_nodeenv,
+    run_and_capture_in_nodeenv)
 
 NODEENV_VERSION = '1.3.3'
-POPCODE_ROOT = path.abspath(path.join(path.dirname(__file__), '..'))
 
-def normalize_version_string(version_string):
+def _normalize_version_string(version_string):
     return re.sub('^\\D+', '', version_string.strip())
 
-with open(path.join(POPCODE_ROOT, 'package.json')) as package_json:
+with open(os.path.join(POPCODE_ROOT, 'package.json')) as package_json:
     engines = json.load(package_json)['engines']
-    NODE_VERSION = normalize_version_string(engines['node'])
-    YARN_VERSION =  normalize_version_string(engines['yarn'])
+    NODE_VERSION = _normalize_version_string(engines['node'])
+    YARN_VERSION =  _normalize_version_string(engines['yarn'])
 
-def is_nodeenv_installed():
-    return path.exists(NODEENV_DIR)
+def _is_nodeenv_installed():
+    return os.path.exists(NODEENV_DIR)
 
-def is_nodeenv_node_version_correct():
-    version = normalize_version_string(run_in_nodeenv('node --version'))
+def _is_nodeenv_node_version_correct():
+    version = _normalize_version_string(
+        run_and_capture_in_nodeenv(['node', '--version']))
     return version == NODE_VERSION
 
-def install_nodeenv():
+def _install_nodeenv():
     nodeenv_tmpdir = tempfile.mkdtemp()
     nodeenv_zip_response = urlopen(
         'https://github.com/ekalinin/nodeenv/archive/'
@@ -45,28 +49,67 @@ def install_nodeenv():
     nodeenv_zip_response.close()
     nodeenv_zip = ZipFile(nodeenv_zip_bytes)
     nodeenv_zip.extractall(nodeenv_tmpdir)
-    return path.join(nodeenv_tmpdir, 'nodeenv-' + NODEENV_VERSION)
+    return os.path.join(nodeenv_tmpdir, 'nodeenv-' + NODEENV_VERSION)
 
-def create_nodeenv(nodeenv_package_dir):
+def _create_nodeenv(nodeenv_package_dir):
     subprocess.call([
         'python',
-        path.join(nodeenv_package_dir, 'nodeenv.py'),
+        os.path.join(nodeenv_package_dir, 'nodeenv.py'),
         '--node=' + NODE_VERSION, NODEENV_DIR])
 
-def install_dependencies():
-    run_in_nodeenv("""
-        npm config set update-notifier false
-        npm install --quiet --global yarn@{yarn_version}
-        yarn install --frozen-lockfile --non-interactive --no-progress --silent
-    """.format(yarn_version=YARN_VERSION))
+def _install_dependencies():
+    run_in_nodeenv(['npm', 'config', 'set', 'update-notifier', 'false'])
+    run_in_nodeenv(['npm',
+                    'install',
+                    '--quiet',
+                    '--global',
+                    'yarn@{yarn_version}'.format(yarn_version=YARN_VERSION)])
+    run_in_nodeenv(['yarn',
+                    'install',
+                    '--frozen-lockfile',
+                    '--non-interactive',
+                    '--no-progress',
+                    '--silent'])
 
-def main():
-    if is_nodeenv_installed() and not is_nodeenv_node_version_correct():
+def _symlink_vscode_config():
+    vscode_dir = os.path.join(POPCODE_ROOT, '.vscode')
+    if not os.path.exists(vscode_dir):
+        os.symlink(os.path.join('contrib', 'vscode'), vscode_dir)
+        gitignore_path = os.path.join(POPCODE_ROOT, '.git', 'info', 'exclude')
+        if not os.path.exists(os.path.dirname(gitignore_path)):
+            os.mkdir(os.path.dirname(gitignore_path))
+        with open(gitignore_path, 'r') as gitignore_r:
+            needs_vscode_in_gitignore = not '.vscode' in gitignore_r
+        if needs_vscode_in_gitignore:
+            with open(gitignore_path, 'a') as gitignore_a:
+                gitignore_a.writelines([".vscode"])
+
+def _print_success_message():
+    print("""
+================================================================================
+
+Your development environment is ready! To run your development server, type:
+
+  tools/yarn.py start
+
+To run tests in watch mode type:
+
+  tools/yarn.py run autotest.jest
+
+or
+
+  tools/yarn.py run autotest.karma
+""")
+
+def setup():
+    if _is_nodeenv_installed() and not _is_nodeenv_node_version_correct():
         shutil.rmtree(NODEENV_DIR)
-    if not is_nodeenv_installed():
-        nodeenv_package_dir = install_nodeenv()
-        create_nodeenv(nodeenv_package_dir)
-    install_dependencies()
+    if not _is_nodeenv_installed():
+        nodeenv_package_dir = _install_nodeenv()
+        _create_nodeenv(nodeenv_package_dir)
+    _install_dependencies()
+    _symlink_vscode_config()
 
 if __name__ == "__main__":
-    main()
+    setup()
+    _print_success_message()
--- tools/util/__init__.py
+++ tools/util/__init__.py
@@ -1,41 +1,82 @@
 from os import path
 from io import StringIO
-from subprocess import (Popen, PIPE)
+import shlex
+import subprocess
+import sys
 
-NODEENV_DIR = path.join(path.dirname(__file__), '..', '..', 'nodeenv')
+POPCODE_ROOT = path.abspath(path.join(path.dirname(__file__), '..', '..'))
+NODEENV_DIR = path.join(POPCODE_ROOT, 'nodeenv')
+NODEENV_BASH_ACTIVATE = path.join(NODEENV_DIR, 'bin', 'activate')
+NODEENV_POWERSHELL_ACTIVATE = path.join(NODEENV_DIR, 'Scripts', 'Activate.ps1')
+
+def nodeenv_delegate(executable):
+    sys.exit(run_in_nodeenv([executable, *sys.argv[1:]]))
 
 def run_in_nodeenv(command):
-    if path.exists(path.join(NODEENV_DIR, 'bin')):
+    if _has_bash_nodeenv():
         return _run_in_nodeenv_with_bash(command)
-    elif path.exists(path.join(NODEENV_DIR, 'Scripts')):
+    elif _has_powershell_nodeenv():
         return _run_in_nodeenv_with_powershell(command)
     else:
         raise Exception('nodeenv did not create either a bin/ or a Scripts/ dir.')
 
 def _run_in_nodeenv_with_bash(command):
-    return _run_and_capture_output([
-        'bash',
-        '-c',
-        """
-        . {nodeenv_dir}/bin/activate
-        {command}
-        """.format(nodeenv_dir=NODEENV_DIR, command=command)])
+    return subprocess.call(_generate_bash_script_args(command))
 
 def _run_in_nodeenv_with_powershell(command):
     try:
-        return _run_in_nodeenv_with_powershell_executable(command, 'pwsh')
-    except OSError:
-        return _run_in_nodeenv_with_powershell_executable(command, 'powershell')
+        return subprocess.check_call(
+            _generate_powershell_script_args(command, 'pwsh'))
+    except subprocess.CalledProcessError:
+        return subprocess.check_call(
+            _generate_powershell_script_args(command, 'powershell'))
 
-def _run_in_nodeenv_with_powershell_executable(command, exe):
-    return _run_and_capture_output([
+def run_and_capture_in_nodeenv(command):
+    if _has_bash_nodeenv():
+        return _run_and_capture_in_nodeenv_with_bash(command)
+    elif _has_powershell_nodeenv():
+        return _run_and_capture_in_nodeenv_with_powershell(command)
+    else:
+        raise Exception('nodeenv did not create either a bin/ or a Scripts/ dir.')
+
+def _run_and_capture_in_nodeenv_with_bash(command):
+    return _run_and_capture_output(_generate_bash_script_args(command))
+
+def _run_and_capture_in_nodeenv_with_powershell(command):
+    try:
+        return _run_and_capture_output(
+            _generate_powershell_script_args(command, 'pwsh'))
+    except OSError:
+        return _run_and_capture_output(
+            _generate_powershell_script_args(command, 'powershell'))
+
+def _generate_bash_script_args(command):
+    return [
+        'bash',
+        '-c',
+        """
+        . {activate}
+        {command}
+        """.format(activate=NODEENV_BASH_ACTIVATE, command=shell_join(command))]
+
+def _generate_powershell_script_args(command, exe):
+    return [
         exe,
         '-Command',
         """
-        & {nodeenv_dir}/Scripts/Activate.ps1
+        & {activate}
         {command}
-        """.format(nodeenv_dir=NODEENV_DIR, command=command)])
+        """.format(activate=NODEENV_POWERSHELL_ACTIVATE, command=shell_join(command))]
+
+def _has_bash_nodeenv():
+    return path.exists(NODEENV_BASH_ACTIVATE)
+
+def _has_powershell_nodeenv():
+    return path.exists(NODEENV_POWERSHELL_ACTIVATE)
 
 def _run_and_capture_output(args):
-    (out, _) = Popen(args, stdout=PIPE).communicate()
+    (out, _) = subprocess.Popen(args, stdout=subprocess.PIPE).communicate()
     return out.decode('utf-8')
+
+def shell_join(command):
+    return ' '.join([shlex.quote(arg) for arg in command])
