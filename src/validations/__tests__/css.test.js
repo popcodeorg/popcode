55e2e415f9622fc3c951bf4030547473dfbb0949
Mat Brown <mat.a.brown@gmail.com>
2019-08-28 21:34:09 -0400

    Update CSS tests to return promise

--- src/validations/__tests__/css.test.js
+++ src/validations/__tests__/css.test.js
@@ -4,7 +4,7 @@ import testValidatorAcceptance from './testValidatorAcceptance';
 import validationTest from './validationHelper';
 
 describe('css validation', () => {
-  test('valid flexbox', () => {
+  test('valid flexbox', () =>
     validationTest(
       `.flex-container {
       display: flex;
@@ -19,19 +19,17 @@ describe('css validation', () => {
       order: 2;
     }`,
       css,
-    );
-  });
+    ));
 
-  test('valid filter', () => {
+  test('valid filter', () =>
     validationTest(
       `img {
       filter: grayscale(100%);
     }`,
       css,
-    );
-  });
+    ));
 
-  test('valid text-shadow declaration', () => {
+  test('valid text-shadow declaration', () =>
     validationTest(
       `p {
       text-shadow: rgba(0,0,0,0.1) 0 -5px, rgba(0,0,0,0.1) 0 -1px, \
@@ -39,88 +37,78 @@ describe('css validation', () => {
       rgba(0,0,0,0.1) -1px -1px, rgba(255,255,255,0.1) 1px 1px;
     }`,
       css,
-    );
-  });
+    ));
 
-  test('valid background-position declarations', () => {
+  test('valid background-position declarations', () =>
     validationTest(
       `img {
       background-position-x: 10px;
       background-position-y: 15%;
     }`,
       css,
-    );
-  });
+    ));
 
-  test('bogus flex value', () => {
+  test('bogus flex value', () =>
     validationTest('.flex-item { flex: bogus; }', css, {
       reason: 'invalid-value',
       row: 0,
       payload: {error: 'bogus'},
-    });
-  });
+    }));
 
-  test('fails with bogus filter value', () => {
+  test('fails with bogus filter value', () =>
     validationTest('img { filter: whitescale(100%); }', css, {
       reason: 'invalid-value',
       row: 0,
       payload: {error: 'whitescale('},
-    });
-  });
+    }));
 
-  test('no opening curly brace', () => {
+  test('no opening curly brace', () =>
     validationTest(
       `p
       display: block;`,
       css,
       {reason: 'block-expected', row: 0, payload: {error: 'p'}},
-    );
-  });
+    ));
 
-  test('no closing curly brace', () => {
+  test('no closing curly brace', () =>
     validationTest(
       `p {
       display: block;`,
       css,
       {reason: 'missing-closing-curly', row: 0},
-    );
-  });
+    ));
 
-  test('bogus character in selector', () => {
+  test('bogus character in selector', () =>
     validationTest('p; div { display: block; }', css, {
       reason: 'invalid-token-in-selector',
       row: 0,
       payload: {token: ';'},
-    });
-  });
+    }));
 
-  test('invalid negative value', () => {
+  test('invalid negative value', () =>
     validationTest('p { padding-left: -2px; }', css, {
       reason: 'invalid-negative-value',
       row: 0,
       payload: {error: '-2px'},
-    });
-  });
+    }));
 
-  test('invalid fractional value', () => {
+  test('invalid fractional value', () =>
     validationTest('p { z-index: 2.4; }', css, {
       reason: 'invalid-fractional-value',
       row: 0,
       payload: {error: '2.4'},
-    });
-  });
+    }));
 
-  test('missing semicolon at end of block', () => {
+  test('missing semicolon at end of block', () =>
     validationTest(
       `p {
       display: block
     }`,
       css,
       {reason: 'missing-semicolon', row: 1},
-    );
-  });
+    ));
 
-  test('missing semicolon within block', () => {
+  test('missing semicolon within block', () =>
     validationTest(
       `
       p {
@@ -130,10 +118,9 @@ describe('css validation', () => {
     `,
       css,
       {reason: 'missing-semicolon', row: 2},
-    );
-  });
+    ));
 
-  test('extra tokens after value', () => {
+  test('extra tokens after value', () =>
     validationTest(
       `
       p {
@@ -142,17 +129,15 @@ describe('css validation', () => {
     `,
       css,
       {reason: 'extra-tokens-after-value', row: 2, payload: {token: '5px'}},
-    );
-  });
+    ));
 
-  test('potentially missing semicolon before first line', () => {
+  test('potentially missing semicolon before first line', () =>
     validationTest(
       `button{border:20px solid b;
     }`,
       css,
       {reason: 'extra-tokens-after-value', row: 0, payload: {token: 'b'}},
-    );
-  });
+    ));
 
   test('extra token that is prefix of the beginning of the line', () => {
     validationTest(
@@ -166,12 +151,11 @@ describe('css validation', () => {
     );
   });
 
-  test('thoroughly unparseable CSS', () => {
+  test('thoroughly unparseable CSS', () =>
     validationTest('<a href="http;.facebook.com>', css, {
       reason: 'invalid-token',
       row: 0,
-    });
-  });
+    }));
 
   testValidatorAcceptance(css, 'css');
 });
--- src/validations/__tests__/testValidatorAcceptance.js
+++ src/validations/__tests__/testValidatorAcceptance.js
@@ -3,7 +3,7 @@ import acceptance from './acceptance.json';
 import validationTest from './validationHelper';
 
 export default function testValidatorAcceptance(validator, language) {
-  test.each(acceptance[language])('Acceptance - %i', source => {
-    validationTest(source, validator);
-  });
+  test.each(acceptance[language])('Acceptance - %i', source =>
+    validationTest(source, validator),
+  );
 }
