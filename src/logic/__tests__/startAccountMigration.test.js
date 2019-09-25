826a803b71794a28453825e7119f62293272071a
Jesse Wang <jesse.wang19@gmail.com>
2019-09-24 20:30:33 -0400

    Updated account migration test to generate state via reducers rather than mocking the state object.

--- src/logic/__tests__/startAccountMigration.test.js
+++ src/logic/__tests__/startAccountMigration.test.js
@@ -1,7 +1,7 @@
 import {Observable} from 'rxjs';
 import reduce from 'lodash-es/reduce';
-import reduceRoot from '../../reducers/index';
 
+import reducer from '../../reducers/user';
 import startAccountMigration from '../startAccountMigration';
 import {
   accountMigrationComplete,
@@ -9,6 +9,8 @@ import {
   accountMigrationNeeded,
   accountMigrationUndoPeriodExpired,
   startAccountMigration as startAccountMigrationAction,
+  userAuthenticated,
+  dismissAccountMigration,
 } from '../../actions/user';
 import {migrateAccount} from '../../clients/firebase';
 import {getCurrentAccountMigration} from '../../selectors';
@@ -20,39 +22,38 @@ import {
   userFactory,
 } from '@factories/clients/firebase';
 import {githubProfileFactory} from '@factories/clients/github';
-
 import {firebaseProjectFactory} from '@factories/data/firebase';
 
-// jest.mock('../../actions/user.js');
 jest.mock('../../clients/firebase.js');
 jest.mock('../../selectors');
 jest.mock('../../util/bugsnag.js');
 jest.useFakeTimers();
 
 describe('startAccountMigration', () => {
-  const getState = jest.fn();
   const dispatch = jest.fn();
   const done = jest.fn();
 
   test('not dismissed during undo period, successful migration', async () => {
+    const mockUser = userFactory.build();
     const mockCredential = credentialFactory.build();
     const mockProfile = githubProfileFactory.build();
-
-    // getCurrentAccountMigration.mockReturnValue({
-    //   firebaseCredential: mockCredential,
-    // });
-
-    const state = applyActions(
-        // startAccountMigrationAction(),
-        // accountMigrationUndoPeriodExpired(),
-        accountMigrationNeeded(mockProfile, mockCredential),
-    );
-
-    const mockUser = userFactory.build();
     const mockProjects = [
       firebaseProjectFactory.build(),
       firebaseProjectFactory.build(),
     ];
+
+    getCurrentAccountMigration.mockReturnValue({
+      firebaseCredential: mockCredential,
+    });
+
+    const state = applyActions(
+      userAuthenticated(mockUser, mockCredential),
+      accountMigrationNeeded(mockProfile, mockCredential),
+      startAccountMigrationAction(),
+      accountMigrationUndoPeriodExpired(),
+      accountMigrationComplete(mockUser, mockCredential, mockProjects),
+    );
+
     migrateAccount.mockResolvedValue({
       user: mockUser,
       migratedProjects: mockProjects,
@@ -60,67 +61,77 @@ describe('startAccountMigration', () => {
 
     const emptyAction = new Observable();
     const migrationDone = startAccountMigration.process(
-      {action$: emptyAction, getState},
+      {action$: emptyAction, getState: () => state},
       dispatch,
       done,
     );
     jest.advanceTimersByTime(5000);
     await migrationDone;
 
-    expect(accountMigrationUndoPeriodExpired).toHaveBeenCalledWith();
     expect(migrateAccount).toHaveBeenCalledWith(mockCredential);
-    expect(accountMigrationComplete).toHaveBeenCalledWith(
-      mockUser,
-      mockCredential,
-      mockProjects,
-    );
   });
 
   test('not dismissed during undo period, error in migration', async () => {
+    const mockUser = userFactory.build();
     const mockCredential = credentialFactory.build();
+    const mockProfile = githubProfileFactory.build();
+
     getCurrentAccountMigration.mockReturnValue({
       firebaseCredential: mockCredential,
     });
 
     const migrationError = firebaseErrorFactory.build();
-    migrateAccount.mockRejectedValue(migrationError);
+    const state = applyActions(
+      userAuthenticated(mockUser, mockCredential),
+      accountMigrationNeeded(mockProfile, mockCredential),
+      startAccountMigrationAction(),
+      accountMigrationUndoPeriodExpired(),
+      accountMigrationError(),
+    );
 
+    migrateAccount.mockRejectedValue(migrationError);
     bugsnagClient.notify.mockResolvedValue();
 
     const emptyAction = new Observable();
     const migrationDone = startAccountMigration.process(
-      {action$: emptyAction, getState},
+      {action$: emptyAction, getState: () => state},
       dispatch,
       done,
     );
     jest.advanceTimersByTime(5000);
     await migrationDone;
 
-    expect(accountMigrationUndoPeriodExpired).toHaveBeenCalledWith();
     expect(migrateAccount).toHaveBeenCalledWith(mockCredential);
-    expect(accountMigrationComplete).not.toHaveBeenCalled();
     expect(bugsnagClient.notify).toHaveBeenCalledWith(migrationError);
-    expect(accountMigrationError).toHaveBeenCalledWith(migrationError);
   });
 
   test('dismissed during undo period', async () => {
+    const mockUser = userFactory.build();
+    const mockCredential = credentialFactory.build();
+    const mockProfile = githubProfileFactory.build();
+
     const cancelAction = new Observable(subscriber => {
       subscriber.next({type: 'DISMISS_ACCOUNT_MIGRATION'});
       subscriber.complete();
     });
+
+    const state = applyActions(
+      userAuthenticated(mockUser, mockCredential),
+      accountMigrationNeeded(mockProfile, mockCredential),
+      startAccountMigrationAction(),
+      dismissAccountMigration(),
+    );
+
     await startAccountMigration.process(
-      {action$: cancelAction, getState},
+      {action$: cancelAction, getState: () => state},
       dispatch,
       done,
     );
-    expect(accountMigrationUndoPeriodExpired).not.toHaveBeenCalled();
+
+    expect(migrateAccount).not.toHaveBeenCalledWith(mockCredential);
   });
 });
 
 function applyActions(...actions) {
-  return reduce(
-      actions,
-      (state, action) => reduceRoot(state, action),
-      undefined,
-  );
+  return reduce(actions, (state, action) => reducer(state, action), undefined);
 }
