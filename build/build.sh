1492f2f9e0d2aa4a01fcc20761dba70fea0b5cf0
Mat Brown <mat.a.brown@gmail.com>
2019-09-27 07:11:23 -0400

    Pass new Firebase configuration variables into build container

--- build/build.sh
+++ build/build.sh
@@ -6,8 +6,10 @@ docker run \
     --rm \
     --env NODE_ENV=production \
     --env FIREBASE_APP \
+    --env FIREBASE_APP_ID \
     --env FIREBASE_API_KEY \
     --env FIREBASE_CLIENT_ID \
+    --env FIREBASE_PROJECT_ID \
     --env GIT_REVISION="$TRAVIS_COMMIT" \
     --env GOOGLE_ANALYTICS_TRACKING_ID \
     --volume="$TRAVIS_BUILD_DIR/dist:/app/dist" \
