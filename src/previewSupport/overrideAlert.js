6ceefc8f5f8b385e20e45540b5e9d57c3a62797e
Mat Brown <mat.a.brown@gmail.com>
2019-11-10 11:32:38 -0500

    Use Swal.fire per breaking changes

--- src/previewSupport/overrideAlert.js
+++ src/previewSupport/overrideAlert.js
@@ -1,10 +1,10 @@
-import swal from 'sweetalert2';
+import Swal from 'sweetalert2';
 
 export default function overrideAlert() {
   Object.defineProperties(window, {
     alert: {
       value: message => {
-        swal(String(message));
+        Swal.fire({text: String(message)});
       },
       configurable: true,
     },
