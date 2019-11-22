2fe415dca4932e3e6869afb77af501ffd62e5dc6
alecmerdler <alecmerdler@gmail.com>
2019-11-22 00:29:30 -0500

    refactor reducers/clients.js to use handleActions

--- src/reducers/__tests__/clients.test.jsx
+++ src/reducers/__tests__/clients.test.jsx
@@ -18,47 +18,78 @@ import {
 } from '../../actions/assignments';
 
 test('createSnapshot sets exportingSnapshot to true', () => {
-  expect(applyActions(createSnapshot()).getIn(['firebase', 'exportingSnapshot'])).toEqual(true);
+  expect(
+    applyActions(createSnapshot()).getIn(['firebase', 'exportingSnapshot']),
+  ).toEqual(true);
 });
 
 test('snapshotCreated sets exportingSnapshot to false', () => {
-  expect(applyActions(snapshotCreated()).getIn(['firebase', 'exportingSnapshot'])).toEqual(false);
+  expect(
+    applyActions(snapshotCreated()).getIn(['firebase', 'exportingSnapshot']),
+  ).toEqual(false);
 });
 
 test('snapshotExportError sets exportingSnapshot to false', () => {
-  expect(applyActions(snapshotExportError()).getIn(['firebase', 'exportingSnapshot'])).toEqual(false);
+  expect(
+    applyActions(snapshotExportError()).getIn([
+      'firebase',
+      'exportingSnapshot',
+    ]),
+  ).toEqual(false);
 });
 
 test('exportProject sets export status to waiting', () => {
-  expect(applyActions(exportProject('gist')).getIn(['projectExports', 'gist', 'status'])).toEqual('waiting');
+  expect(
+    applyActions(exportProject('gist')).getIn([
+      'projectExports',
+      'gist',
+      'status',
+    ]),
+  ).toEqual('waiting');
 });
 
 test('projectExported sets export status to ready', () => {
-  expect(applyActions(projectExported('example.com', 'gist', '', null)).getIn(['projectExports', 'gist', 'status'])).toEqual('ready');
+  expect(
+    applyActions(projectExported('example.com', 'gist', '', null)).getIn([
+      'projectExports',
+      'gist',
+      'status',
+    ]),
+  ).toEqual('ready');
 });
 
 test('projectExportError sets export status to error', () => {
-  expect(applyActions(projectExportError('gist')).getIn(['projectExports', 'gist', 'status'])).toEqual('error');
+  expect(
+    applyActions(projectExportError('gist')).getIn([
+      'projectExports',
+      'gist',
+      'status',
+    ]),
+  ).toEqual('error');
 });
 
 test('gapiClientReady sets gapi ready to true', () => {
-  expect(applyActions(gapiClientReady('gist')).getIn(['gapi', 'ready'])).toEqual(true);
-});
-
-test('gapiClientReady sets gapi ready to true', () => {
-  expect(applyActions(gapiClientReady('gist')).getIn(['gapi', 'ready'])).toEqual(true);
+  expect(
+    applyActions(gapiClientReady('gist')).getIn(['gapi', 'ready']),
+  ).toEqual(true);
 });
 
 test('createAssignment sets exportingAssignment to true', () => {
-  expect(applyActions(createAssignment()).get('exportingAssignment')).toEqual(true);
+  expect(applyActions(createAssignment()).get('exportingAssignment')).toEqual(
+    true,
+  );
 });
 
 test('assignmentCreated sets exportingAssignment to true', () => {
-  expect(applyActions(assignmentCreated()).get('exportingAssignment')).toEqual(false);
+  expect(applyActions(assignmentCreated()).get('exportingAssignment')).toEqual(
+    false,
+  );
 });
 
 test('assignmentNotCreated sets exportingAssignment to true', () => {
-  expect(applyActions(assignmentNotCreated()).get('exportingAssignment')).toEqual(false);
+  expect(
+    applyActions(assignmentNotCreated()).get('exportingAssignment'),
+  ).toEqual(false);
 });
 
 function applyActions(...actions) {
