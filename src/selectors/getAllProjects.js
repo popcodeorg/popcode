8a44a5875b94101a2e1a1e86f465c978a0bc7250
Peter Jablonski <pwjablonski@gmail.com>
2020-01-20 17:50:21 -0500

    sets current project to top of list on new project

--- src/selectors/getAllProjects.js
+++ src/selectors/getAllProjects.js
@@ -1,8 +1,12 @@
+import isNil from 'lodash-es/isNil';
 import sortBy from 'lodash-es/sortBy';
 import values from 'lodash-es/values';
 import {createSelector} from 'reselect';
 
 export default createSelector(
   state => state.get('projects'),
-  projects => sortBy(values(projects.toJS()), project => -project.updatedAt),
+  projects =>
+    sortBy(values(projects.toJS()), ({updatedAt}) =>
+      isNil(updatedAt) ? -Number.MAX_VALUE : -updatedAt,
+    ),
 );
