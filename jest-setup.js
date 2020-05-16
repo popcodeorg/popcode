f5e7d1b8520489f8931cbce1e1dd63e1a179d349
Mat Brown <mat.a.brown@gmail.com>
2020-05-16 12:17:41 -0400

    Port calculateFlexGrowAfterDrag test

--- jest-setup.js
+++ jest-setup.js
@@ -1,7 +1,32 @@
+import almostEqual from 'almost-equal';
 // eslint-disable-next-line import/no-namespace
 import * as immutableMatchers from 'jest-immutable-matchers';
+import every from 'lodash-es/every';
+import zip from 'lodash-es/zip';
 
 jest.addMatchers(immutableMatchers);
+
+expect.extend({
+  toHaveAlmostEqualElements(received, expected) {
+    const pass =
+      received.length === expected.length &&
+      every(zip(received, expected), ([value1, value2]) =>
+        almostEqual(value1, value2),
+      );
+    if (pass) {
+      return {
+        pass: true,
+        message: () =>
+          `expected ${received} not to contain approximately equal elements to ${expected}`,
+      };
+    }
+    return {
+      pass: false,
+      message: () =>
+        `expected ${received} to contain approximately equal elements to ${expected}`,
+    };
+  },
+});
 
 function createRange() {
   // createRange is always called with a Document context
