b50ee9567b8cc124c319b1841600196b036ab900
Mat Brown <mat.a.brown@gmail.com>
2020-07-30 17:36:58 -0400

    Ignore bower_components in Jest module loader

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     '@factories/(.*)$': '<rootDir>/__factories__/$1',
     '\\.(html|svg)': '<rootDir>/__mocks__/fileMock.js',
   },
+  modulePathIgnorePatterns: ['<rootDir>/bower_components/'],
   testMatch: ['**/__tests__/**/*.test.js?(x)'],
   testPathIgnorePatterns: ['/node_modules/', '/bower_components/', '/nodeenv/'],
   transformIgnorePatterns: ['node_modules/(?!(lodash-es)/)'],
